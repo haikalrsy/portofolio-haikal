7d4595411f9458965d51de9025df4a0374040e3c
haikalrsy <haikalrasyaputra@gmail.com>
2025-07-24 14:14:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portofolio-haikal/", // ← sesuai dengan nama repo GitHub kamu
+  base: "/", // ← sesuai dengan nama repo GitHub kamu
   plugins: [react()],
 });
